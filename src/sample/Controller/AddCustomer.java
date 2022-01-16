d6dd31bbd65ed35fb83e315b49aa95b9da1c28f3
ewn2 <erwin.uppal@gmail.com>
2022-01-16 00:28:47 +0000

    Add and Modify Customer Views

--- src/sample/Controller/AddCustomer.java
+++ src/sample/Controller/AddCustomer.java
@@ -1,4 +1,27 @@
 package sample.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class AddCustomer {
+    public TextField addCustomerNameBox;
+    public TextField addCustomerPhoneBox;
+    public ComboBox addCustomerCountryCombo;
+    public TextField addCustomerPostalBox;
+    public ComboBox addCustomerStateCombo;
+    public TextField addCustomerCityBox;
+    public TextField addCustomerAddressBox;
+    public Button addCustomerSaveButton;
+    public Button addCustomerCancelButton;
+    public TextArea errorMessageBox;
+    public TextField addCustomerIDBox;
+
+    public void onAddCustomerSaveButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void onAddCustomerCancelButtonAction(ActionEvent actionEvent) {
+    }
 }
--- src/sample/Controller/ModifyCustomer.java
+++ src/sample/Controller/ModifyCustomer.java
@@ -1,4 +1,27 @@
 package sample.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class ModifyCustomer {
+    public TextField ModifyCustomerNameBox;
+    public TextField ModifyCustomerPhoneBox;
+    public ComboBox ModifyCustomerCountryCombo;
+    public TextField ModifyCustomerPostalBox;
+    public ComboBox ModifyCustomerStateCombo;
+    public TextField ModifyCustomerCityBox;
+    public TextField ModifyCustomerAddressBox;
+    public Button ModifyCustomerSaveButton;
+    public Button ModifyCustomerCancelButton;
+    public TextArea errorMessageBox;
+    public TextField ModifyCustomerIDBox;
+
+    public void onModifyCustomerSaveButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void onModifyCustomerCancelButtonAction(ActionEvent actionEvent) {
+    }
 }
